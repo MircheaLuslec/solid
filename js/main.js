bd623a5b444fac622bcdd036c0d860cfdaa40fde
MircheaLuslec <mlfeed31mr@gmail.com>
2024-12-31 15:41:10 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 2000, // Автоматическое переключение слайдов через 2.5 секунды
+    },
+    spaceBetween: 30, // Расстояние между слайдами
+  });
